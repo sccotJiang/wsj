eaec4f285dc40a856a7bfea5ef12bbef4c229cf2
yixiao <1961373465@qq.com>
2023-04-18 21:38:18 +0800

    一些包

--- cmd/websocket/main.go
+++ cmd/websocket/main.go
@@ -1,15 +1,23 @@
-package websocket
+package main
 
 import (
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
+
+func init() {
+
+}
 
 func main() {
 	go listenSyscall()
+	<-time.After(time.Second * 50)
 }
+
+//监听系统信号
 func listenSyscall() {
 	sigc := make(chan os.Signal, 1)
 	//监听和捕获信号量。首先定义一个c，用于传递信号量，然后指定哪些信号量是需要被捕获的，如果不指定，就会捕获任何信号量。
@@ -21,5 +29,6 @@ func listenSyscall() {
 	select {
 	case sig := <-sigc:
 		log.Printf("receive system signal %v, start terminating", sig)
+
 	}
 }
